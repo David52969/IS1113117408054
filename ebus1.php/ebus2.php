a377f57161afdabd8943ee48130fdd0a82ac2753
David <117408054@umail.ucc.ie>
2018-03-05 12:04:43 +0000

    commit

--- ebus1.php/ebus2.php
+++ ebus1.php/ebus2.php
@@ -2,47 +2,18 @@
 <!DOCTYPE HTML>  
 <html>
 <head>
-<style>
-.error {color: #FF0000;}
-</style>
 </head>
 <body>  
 
 <?php
-
-$nameErr = $emailErr = $genderErr = "";
-$name = $email = $gender = $comment = "";
+// define variables and set to empty values
+$name = $email = $gender = $comment =  "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
 }
 
 function test_input($data) {
@@ -53,36 +24,31 @@ function test_input($data) {
 }
 ?>
 
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field.</span></p>
+<h2>Personal Details</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name">
-  <span class="error">* <?php echo $nameErr;?></span>
   <br><br>
   E-mail: <input type="text" name="email">
-  <span class="error">* <?php echo $emailErr;?></span>
   <br><br>
   Comment: <textarea name="comment" rows="5" cols="40"></textarea>
   <br><br>
   Gender:
   <input type="radio" name="gender" value="female">Female
   <input type="radio" name="gender" value="male">Male
-  <span class="error">* <?php echo $genderErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
 
 <?php
-echo "<h2>Your Details:</h2>";
+echo "<h2>Your Receipt:</h2>";
 echo $name;
 echo "<br>";
 echo $email;
 echo "<br>";
+echo $comment;
+echo "<br>";
 echo $gender;
 ?>
-          <form action="ehome.html">
-    <a role="button" href="ehome.html">Home/a>
-</form>
 
 </body>
 </html
